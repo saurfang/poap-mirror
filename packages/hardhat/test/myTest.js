d33a74e04b5b28f807c544b02f26ced39eab29d2
Forest Fang <forestfang@stripe.com>
2022-11-06 01:27:31 -0700

    get test working

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -1,6 +1,7 @@
 const { ethers } = require("hardhat");
 const { use, expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
+const { CCIPReadProvider } = require("@chainlink/ethers-ccip-read-provider");
 
 use(solidity);
 
@@ -9,17 +10,21 @@ describe("My Dapp", function () {
 
   describe("YourContract", function () {
     it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
+      const ERC721Mirror = await ethers.getContractFactory("ERC721Mirror");
 
-      myContract = await YourContract.deploy();
+      myContract = await ERC721Mirror.deploy("Token", "TOKEN");
+      await myContract.setUrls(["https://DarkHalfProfile.saurfang1.repl.co"]);
+      myContract = new ethers.Contract(
+        myContract.address,
+        myContract.interface,
+        new CCIPReadProvider(myContract.provider)
+      );
     });
 
-    describe("setPurpose()", function () {
-      it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
-
-        await myContract.setPurpose(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
+    describe("balanceOf()", function () {
+      it("Should be able to query owner off chain", async function () {
+        const owner = await myContract.ownerOf(533809);
+        expect(owner).to.equal("0xe459FCBA06Bb8210de26D20C328d49e687ACC34b");
       });
     });
   });
