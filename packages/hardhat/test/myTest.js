ed7b13acef1376d105dbec6c2e6bbbb79d737f90
Forest Fang <forestfang@stripe.com>
2022-11-06 02:10:06 -0800

    update test

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -8,13 +8,15 @@ use(solidity);
 describe("My Dapp", function () {
   let myContract;
 
-  describe("YourContract", function () {
-    it("Should deploy YourContract", async function () {
+  describe("ERC721Mirror", function () {
+    it("Should deploy ERC721Mirror", async function () {
       const ERC721Mirror = await ethers.getContractFactory("ERC721Mirror");
 
       myContract = await ERC721Mirror.deploy("Token", "TOKEN");
       // await myContract.setUrls(["https://DarkHalfProfile.saurfang1.repl.co"]);
-      await myContract.setUrls(["https://fabuloussnowapplicationstack.ericzhang98.repl.co"]);
+      await myContract.setUrls([
+        "https://fabuloussnowapplicationstack.ericzhang98.repl.co",
+      ]);
       myContract = new ethers.Contract(
         myContract.address,
         myContract.interface,
@@ -22,21 +24,19 @@ describe("My Dapp", function () {
       );
     });
 
-    describe("balanceOf()", function () {
-      it("Should be able to query owner off chain", async function () {
-        const owner = await myContract.ownerOf(559572);
-        expect(owner).to.equal("0x501783E585936116220b5028C4C22DC9fdB991Bc");
-      });
-      it("Should be able to query balance off chain", async function () {
-        const balance = await myContract.balanceOf(
-          "0x501783E585936116220b5028C4C22DC9fdB991Bc"
-        );
-        expect(balance).to.equal(1255);
-      });
-      it("Should be able to query tokenUri off chain", async function () {
-        const url = await myContract.tokenURI(559572);
-        expect(url).to.equal("https://api.poap.xyz/metadata/3671/559572");
-      });
+    it("Should be able to query owner off chain", async function () {
+      const owner = await myContract.ownerOf(559572);
+      expect(owner).to.equal("0x501783E585936116220b5028C4C22DC9fdB991Bc");
+    });
+    it("Should be able to query balance off chain", async function () {
+      const balance = await myContract.balanceOf(
+        "0x501783E585936116220b5028C4C22DC9fdB991Bc"
+      );
+      expect(balance).to.equal(1255);
+    });
+    it("Should be able to query tokenUri off chain", async function () {
+      const url = await myContract.tokenURI(559572);
+      expect(url).to.equal("https://api.poap.xyz/metadata/3671/559572");
     });
   });
 });
